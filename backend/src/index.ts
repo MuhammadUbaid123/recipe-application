4a5ef003ba09871afc93d353fc3675a915bf97b9
MuhammadUbaid123 <116624311+Ubaidnawaz@users.noreply.github.com>
2025-05-08 16:44:42 +0500

    Initial commit Setting up project

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/api/recipes/search", async (req, res)=>{
+    res.json({
+        message: "Success!"
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
